d66202e88ad6596f6d8a1def179adb6fa478f6c2
l46kok <l46kok@localhost>
2016-07-06 09:38:13 -0600

    Fate Webserver Initial Commit

---
+++ Executable/MainBootstrapper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+using Nancy.Conventions;
+
+namespace Executable
+{
+    public class MainBootstrapper : DefaultNancyBootstrapper
+    {
+        protected override void ConfigureConventions(NancyConventions nancyConventions)
+        {
+            base.ConfigureConventions(nancyConventions);
+
+            Conventions.StaticContentsConventions.Add(
+                StaticContentConventionBuilder.AddDirectory("/", "Assets")
+            );
+        }
+    }
+}
---
+++ Executable/Utility/NancyExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Executable.Utility
+{
+    public static class NancyExtensions
+    {
+        public static Response AsError(this IResponseFormatter formatter, HttpStatusCode statusCode, string message)
+        {
+            return new Response
+            {
+                StatusCode = statusCode,
+                ContentType = "text/plain",
+                Contents = stream => (new StreamWriter(stream) { AutoFlush = true }).Write(message)
+            };
+        }
+    }
+}
