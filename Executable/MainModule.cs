6ec3aa427d4e5497ea4fe20d4edae780de4efab0
l46kok <2355949+l46kok@users.noreply.github.com>
2016-07-06 09:38:13 -0600

    Fate Webserver Initial Commit

---
+++ Executable/MainModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Executable
+{
+    public class MainModule : NancyModule
+    {
+        public MainModule()
+        {
+            Get["/"] = _ => View["Views/MainPage.html"];
+            Get["/SearchPlayer"] = _ => View["Views/SearchPlayer.html"];
+            Get["/SearchGame"] = _ => View["Views/SearchGame.html"];
+            Get["/Home"] = _ => View["Views/Home.html"];
+            Get["/About"] = _ => View["Views/About.html"];
+            Get["/PlayerStats"] = _ => View["Views/PlayerStats.html"];
+        }
+    }
+}
---
+++ Executable/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace Executable
+{
+    class Program
+    {
+        const ushort Port = 64402;
+        const string EscapeString = "/Terminate";
+
+        static void Main()
+        {
+            NancyHost host;
+
+            #region Making new instance of NancyHost
+            var uri = new Uri("http://localhost:" + Port + "/");
+            var config = new HostConfiguration(); config.UrlReservations.CreateAutomatically = true;
+
+            host = new NancyHost(config, uri);
+            #endregion
+            #region NancyFX hosting loop
+            try
+            {
+                host.Start();
+
+                Console.Write("Start hosting the Fate/Another ranking system frontend\n" +
+                    "\t\"" + uri + "\"\n" +
+                    "To stop the hosting, input \"" + EscapeString + "\".\n\n");
+                do Console.Write("> "); while (Console.ReadLine() != EscapeString) ;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unhandled exception has been occured!\n"
+                    + e.Message);
+                Console.ReadKey(true);
+            }
+            finally
+            {
+                host.Stop();
+            }
+
+            Console.WriteLine("Goodbye");
+            #endregion
+        }
+    }
+}
