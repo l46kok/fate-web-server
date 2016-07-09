b36735ebae405ef6d3324a065b669db9d2821e33
l46kok <2355949+l46kok@users.noreply.github.com>
2016-07-08 23:36:34 -0600

    Added PlayerStatsPage template

--- Executable/MainBootstrapper.cs
+++ Executable/MainBootstrapper.cs
@@ -17,17 +17,17 @@ namespace FateWebServer
             base.ConfigureConventions(nancyConventions);
 
             StaticConfiguration.DisableErrorTraces = false;
+            favicon = LoadFavIcon();
         }
 
         protected override byte[] FavIcon
         {
-            /*get { return this.favicon ?? (this.favicon = null); }*/
-            get { return null; }
+            get { return this.favicon ?? (this.favicon = null); }
         }
 
         private byte[] LoadFavIcon()
         {
-            using (var resourceStream = GetType().Assembly.GetManifestResourceStream("AssemblyName.favicon.ico"))
+            using (var resourceStream = GetType().Assembly.GetManifestResourceStream("FateWebServer.favicon.ico"))
             {
                 var memoryStream = new MemoryStream();
                 resourceStream.CopyTo(memoryStream);
---
+++ Executable/ViewModels/PlayerStatsPageViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace FateWebServer.ViewModels
+{
+    public class PlayerStatsPageViewModel
+    {
+        public bool HasFoundUser { get; set; }
+        public string UserName { get; set; }
+    }
+}
