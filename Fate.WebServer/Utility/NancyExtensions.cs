07f09cc242a9c1e30c42cac1588d66710c6fe930
l46kok <l46kok@localhost>
2016-08-19 03:17:15 -0600

    Renamed Project Directory Name (Executable->Fate.WebServer)

--- Fate.WebServer/Utility/NancyExtensions.cs
+++ Fate.WebServer/Utility/NancyExtensions.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 using Nancy;
 
-namespace Executable.Utility
+namespace FateWebServer.Utility
 {
     public static class NancyExtensions
     {
