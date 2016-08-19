6d8f88cd9933435f535f7757031464a9e9df3681
l46kok <2355949+l46kok@users.noreply.github.com>
2016-08-19 03:08:46 -0600

    DB Initialization code added in program.cs, config.cfg template added

--- Executable/Program.cs
+++ Executable/Program.cs
@@ -1,17 +1,34 @@
 ﻿using System;
+using Fate.DB;
+using FateWebServer.Utility;
 using Nancy.Hosting.Self;
 
 namespace FateWebServer
 {
     static class Program
     {
-        const ushort Port = 64402;
-        const string EscapeString = "/Terminate";
+        private const ushort WEB_SERVER_PORT = 64402;
+        private const string TERMINATE_STRING = "/Terminate";
+        private const string DEFAULT_CONFIG_FILE_PATH = "config.cfg";
 
         static void Main()
         {
+            ConfigHandler configHandler = new ConfigHandler(DEFAULT_CONFIG_FILE_PATH);
+            if (!configHandler.IsConfigFileValid())
+                Console.WriteLine("Error loading default config file: {0}", DEFAULT_CONFIG_FILE_PATH);
+            else
+            {
+                Console.WriteLine("Loading default config file: {0}", DEFAULT_CONFIG_FILE_PATH);
+                configHandler.LoadConfig();
+                frsDb.InitDatabaseConnection(configHandler.DatabaseServer,
+                                             configHandler.DatabasePort,
+                                             configHandler.DatabaseUserName,
+                                             configHandler.DatabasePassword,
+                                             configHandler.DatabaseName);
+            }
+
             #region Making new instance of NancyHost
-            var uri = new Uri("http://localhost:" + Port + "/");
+            var uri = new Uri("http://localhost:" + WEB_SERVER_PORT + "/");
             var config = new HostConfiguration
             {
                 UrlReservations = {CreateAutomatically = true},
@@ -27,8 +44,8 @@ namespace FateWebServer
 
                 Console.Write("Fate/Another III Ranking System Web Server\n" +
                     "\t\"" + uri + "\"\n" +
-                    "To quit, input \"" + EscapeString + "\".\n\n");
-                do Console.Write("> "); while (Console.ReadLine() != EscapeString) ;
+                    "To quit, input \"" + TERMINATE_STRING + "\".\n\n");
+                do Console.Write("> "); while (Console.ReadLine() != TERMINATE_STRING) ;
             }
             catch (Exception e)
             {
