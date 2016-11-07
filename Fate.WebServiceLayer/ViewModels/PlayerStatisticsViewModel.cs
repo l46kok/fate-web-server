52a36f7e2ad9d72e78a20f334f50e67020cb85ec
l46kok <2355949+l46kok@users.noreply.github.com>
2016-11-07 03:10:34 -0700

    Player Statistics Completed

---
+++ Fate.WebServiceLayer/ViewModels/PlayerStatisticsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fate.WebServiceLayer.ViewModels
+{
+    public class PlayerStatisticsViewModel
+    {
+        public string PlayerName { get; set; }
+        public string WinRatio { get; set; }
+        public string WinRatioPBColor { get; set; }
+        public string AvgKDA { get; set; }
+        public string KDAColor { get; set; }
+        public string AvgGoldSpent { get; set; }
+        public string AvgDamageDealt { get; set; }
+        public string AvgDamageTaken { get; set; }
+    }
+}
