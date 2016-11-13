ec2a1c766087a101ab4edd77070f0eb5f467d329
l46kok <l46kok@localhost>
2016-11-12 22:10:25 -0700

    Fixed leaderboard refresh method

--- Fate.WebServer/Content/scripts/LeaderBoards.js
+++ Fate.WebServer/Content/scripts/LeaderBoards.js
@@ -20,56 +20,20 @@ function refreshStatistics() {
     $.get("/PlayerStatsAjax/Statistics/" + filterType + "/" + timeType, function (data) {
         $.each(data, function (idx, elem) {
             var tableRow = $('<tr></tr>');
-            var playerTd = $('<td>' + elem.playerName + '</td>');
-            var winRatioTd = $('<td class="leftText"><div class="' + elem.winRatioPBColor + '" style="width: ' + elem.winRatio + '"</div><span class="winrateval">' + elem.winRatio + '</span></td>');
+            var playerTd = $('<td><b><a href="/PlayerStats/USEast/' + elem.playerName + '">' + elem.playerName + '</a></b></td>');
+            var winRatioTd = $('<td class="leftText"><div class="' + elem.winRatioPBColor + '" style="width: ' + elem.winRatio + '"></div><span class="winrateval">' + elem.winRatio + '</span></td>');
+            var kdaTd = $('<td class="' + elem.kDAColor + '"><b>' + elem.avgKDA + '</b></td>');
+            var damageDealtTd = $('<td>' + elem.avgDamageDealt + '</td>');
+            var damageTakenTd = $('<td>' + elem.avgDamageTaken + '</td>');
+            var avgGoldSpentTd = $('<td>' + elem.avgGoldSpent + '</td>');
             tableRow.append(playerTd);
             tableRow.append(winRatioTd);
+            tableRow.append(kdaTd);
+            tableRow.append(damageDealtTd);
+            tableRow.append(damageTakenTd);
+            tableRow.append(avgGoldSpentTd);
             tableBody.append(tableRow);
         });
-        // <td>Player1</td>
-        // <td class="leftText"><div class="progressbar blue" style="width: 77%"></div><span class="winrateval">77.5%</span></td> 
-        // <td>2.42 : 1</td> 
-        // <td>144,444</td> 
-        // <td>999,999</td> 
-        // <td>999,999</td> 
-
-        // $.each(data, function (idx, elem) {
-        // var statsBox = $('<div class="statsBox"></div>');
-        // var gameStatsBox = $('<div class="gameStatsBox hoverDiv" data-toggle="collapse" data-target="#collapsible' + elem.gameID + '"></div>');
-        // var gameResultInfo = $('<div class="gameTableCell centerText gameResultInfo">');
-        // gameResultInfo.append('<div class="' + elem.gameResult + ' GameResult">' + elem.gameResult + '(' + elem.teamOneWinCount + ' - ' + elem.teamTwoWinCount + ')</div>');
-        // gameResultInfo.append('<div>' + elem.playedDate + '</div>');
-        // gameStatsBox.append(gameResultInfo);
-
-        // avgDamageDealt
-        // :
-        // "75,757"
-        // avgDamageTaken
-        // :
-        // "60,701"
-        // avgGoldSpent
-        // :
-        // "132,254"
-        // avgKDA
-        // :
-        // "5.33"
-        // kDAColor
-        // :
-        // "kdagold"
-        // playerName
-        // :
-        // "CharismaPeon"
-        // winRatio
-        // :
-        // "83.3%"
-
-        // debugger;
-
-        // let the plugin know that we made a update 
         $("#leaderBoardsTable").trigger("update");
-        // set sorting column and direction, this will sort on the first and third column 
-        //var sorting = [[2,1],[0,0]]; 
-        // sort on the first column 
-        //$("#leaderBoardsTable").trigger("sorton",[sorting]); 
     });
 }
