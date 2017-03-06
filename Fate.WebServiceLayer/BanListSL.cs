bef101e75d84971154b9f3ac5d0673e624b993f7
l46kok <2355949+l46kok@users.noreply.github.com>
2017-03-05 20:33:04 -0500

    * Implemented banlist page

---
+++ Fate.WebServiceLayer/BanListSL.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Fate.DB.DAL.FRS;
+using Fate.WebServiceLayer.ViewModels;
+
+namespace Fate.WebServiceLayer
+{
+    public class BanListSL
+    {
+        private static readonly BanDAL _banDal = new BanDAL();
+        public static BanListSL Instance { get; } = new BanListSL();
+
+        public BanListViewModel GetBannedPlayers()
+        {
+            var playerBanDataList = _banDal.GetCurrentlyBannedPlayers(false).ToList();
+            BanListViewModel banListViewModel = new BanListViewModel
+            {
+                PermanentBans = playerBanDataList.Where(x => x.IsPermanentBan),
+                TimeBans = playerBanDataList.Where(x => !x.IsPermanentBan).ToList()
+            };
+            banListViewModel.TimeBans.ToList().ForEach(x =>
+            {
+                if (x.BannedUntil != null)
+                    x.RemainingDuration = x.BannedUntil.Value.Subtract(DateTime.Now).Minutes;
+            });
+            return banListViewModel;
+        }
+    }
+}
